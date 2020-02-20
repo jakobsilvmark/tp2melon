1d07fd8fd43d97077e7f778fed7286513d08c500
phani192 <54536657+phani192@users.noreply.github.com>
2020-02-20 10:30:13 +0100

    rough

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from '../Navigation';
+const App =() => (
+<router>
+    <Navigation/>
+</router>
+
+);
+export default App;
